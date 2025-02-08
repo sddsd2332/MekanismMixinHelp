f5fa79b374c0ccbc8a1eca6aa4790ae4e0251a40
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2025-02-08 20:31:40 +0800

    Adjustments are injected to the beginning

--- src/main/java/mekmixinhelp/mixin/minecraft/MinxinEntityLivingBase.java
+++ src/main/java/mekmixinhelp/mixin/minecraft/MinxinEntityLivingBase.java
@@ -11,8 +11,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityLivingBase.class)
 public abstract class MinxinEntityLivingBase extends Entity {
@@ -23,37 +25,31 @@ public abstract class MinxinEntityLivingBase extends Entity {
     @Shadow
     public int ticksElytraFlying;
 
-    @Shadow
-    public abstract boolean isPotionActive(Potion potionIn);
+    @Shadow public abstract boolean isPotionActive(Potion potionIn);
 
     public MinxinEntityLivingBase(World worldIn) {
         super(worldIn);
     }
 
-    /**
-     * @author sddsd2332
-     * @reason 修改鞘翅更新逻辑，与高版本一致
-     */
-    @Overwrite
-    public void updateElytra() {
+    @Inject(method = "updateElytra", at = @At(value = "HEAD"),cancellable = true)
+    public void isElytra(CallbackInfo ci) {
         boolean flag = this.getFlag(7);
         if (flag && !this.onGround && !this.isRiding() && !this.isPotionActive(MobEffects.LEVITATION)) {
             ItemStack itemstack = this.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
-            if (itemstack.getItem() == Items.ELYTRA && ItemElytra.isUsable(itemstack)) {
+            if (itemstack.getItem() == Items.ELYTRA && ItemElytra.isUsable(itemstack) || itemstack.getItem() instanceof ElytraMixinHelp help && help.canElytraFly(itemstack, ((EntityLivingBase) (Object) this)) && help.elytraFlightTick(itemstack, ((EntityLivingBase) (Object) this), this.ticksElytraFlying)) {
                 flag = true;
-                if (!this.world.isRemote && (this.ticksElytraFlying + 1) % 20 == 0) {
+                if (!this.world.isRemote && (this.ticksElytraFlying + 1) % 20 == 0 && itemstack.getItem() == Items.ELYTRA) {
                     itemstack.damageItem(1, ((EntityLivingBase) (Object) this));
                 }
             } else {
-                flag = itemstack.getItem() instanceof ElytraMixinHelp help && help.canElytraFly(itemstack, ((EntityLivingBase) (Object) this)) && help.elytraFlightTick(itemstack, ((EntityLivingBase) (Object) this), this.ticksElytraFlying);
+                flag = false;
             }
         } else {
             flag = false;
         }
-
         if (!this.world.isRemote) {
             this.setFlag(7, flag);
         }
+        ci.cancel();
     }
-
 }
