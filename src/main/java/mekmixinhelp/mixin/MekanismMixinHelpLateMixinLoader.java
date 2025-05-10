4328a92fe2eead7fc122f7dd33f26ca1ca8a1716
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2025-05-11 00:13:48 +0800

    添加工厂版本的jei配方显示

--- src/main/java/mekmixinhelp/mixin/MekanismMixinHelpLateMixinLoader.java
+++ src/main/java/mekmixinhelp/mixin/MekanismMixinHelpLateMixinLoader.java
@@ -18,6 +18,8 @@ public class MekanismMixinHelpLateMixinLoader implements ILateMixinLoader {
     static {
         addModdedMixinCFG("mixins.mekmixinhelp.extrabotany.json", "extrabotany");
         addModdedMixinCFG("mixins.mekmixinhelp.tconevo.json", "tconevo");
+        addModdedMixinCFG("mixins.mekmixinhelp.ae2uel.json","appliedenergistics2");
+        addModdedMixinCFG("mixins.mekmixinhelp.jei.json","jei");
     }
 
     @Override
