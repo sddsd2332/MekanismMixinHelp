35a6a56b15dbb69f688602478faa30dc9cd43428
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2026-08-21 23:04:28 +0800

    更新种植机配方注册方式

--- src/main/java/mekmixinhelp/mixin/thermalexpansion/MixinInsolatorManager.java
+++ src/main/java/mekmixinhelp/mixin/thermalexpansion/MixinInsolatorManager.java
@@ -3,15 +3,20 @@ package mekmixinhelp.mixin.thermalexpansion;
 import cofh.thermalexpansion.util.managers.machine.InsolatorManager;
 import cofh.thermalexpansion.util.managers.machine.InsolatorManager.*;
 import cofh.thermalfoundation.item.ItemFertilizer;
+import mekanism.api.recipes.FarmChanceOutput;
 import mekanism.common.MekanismFluids;
 import mekanism.common.config.MekanismConfig;
 import mekanism.common.recipe.RecipeHandler;
 import mekanism.common.util.StackUtils;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Collections;
 
 @Mixin(value = InsolatorManager.class, remap = false)
 public class MixinInsolatorManager {
@@ -20,12 +25,16 @@ public class MixinInsolatorManager {
     private static void addMekanismFarm(int energy, int water, ItemStack primaryInput, ItemStack secondaryInput, ItemStack primaryOutput, ItemStack secondaryOutput, int secondaryChance, Type type, CallbackInfoReturnable<InsolatorRecipe> cir) {
         if (secondaryInput == ItemFertilizer.fertilizerBasic || secondaryInput == ItemFertilizer.fertilizerRich || secondaryInput == ItemFertilizer.fertilizerFlux) {
             if (!RecipeHandler.Recipe.ORGANIC_FARM.containsRecipe(primaryInput)) {
+                int waterMultiplier = type == Type.TREE ? 6 : 3;
                 if (!secondaryOutput.isEmpty()) {
-                    RecipeHandler.addOrganicFarmRecipe(primaryInput, MekanismFluids.NutrientSolution, StackUtils.size(primaryOutput, 24), StackUtils.size(secondaryOutput, 4), type == Type.TREE ? MekanismConfig.current().mekce.log.val() : MekanismConfig.current().mekce.seed.val());
-                    RecipeHandler.addOrganicFarmRecipe(primaryInput, MekanismFluids.Water, StackUtils.size(primaryOutput, 6), StackUtils.size(secondaryOutput, 1), type == Type.TREE ? MekanismConfig.current().mekce.log.val() : MekanismConfig.current().mekce.seed.val());
+                    double chance = type == Type.TREE ? MekanismConfig.current().mekce.log.val() : MekanismConfig.current().mekce.seed.val();
+                    RecipeHandler.addOrganicFarmRecipe(primaryInput, MekanismFluids.NutrientSolution, StackUtils.size(primaryOutput, 24),
+                            Collections.singletonList(new FarmChanceOutput(StackUtils.size(secondaryOutput, 4), chance)));
+                    RecipeHandler.addOrganicFarmRecipe(primaryInput, new FluidStack(FluidRegistry.WATER, 1), StackUtils.size(primaryOutput, waterMultiplier),
+                            Collections.singletonList(new FarmChanceOutput(StackUtils.size(secondaryOutput, 1), chance)));
                 } else {
                     RecipeHandler.addOrganicFarmRecipe(primaryInput, MekanismFluids.NutrientSolution, StackUtils.size(primaryOutput, 24));
-                    RecipeHandler.addOrganicFarmRecipe(primaryInput, MekanismFluids.Water, StackUtils.size(primaryOutput, 6));
+                    RecipeHandler.addOrganicFarmRecipe(primaryInput, new FluidStack(FluidRegistry.WATER, 1), StackUtils.size(primaryOutput, waterMultiplier));
                 }
             }
         }
